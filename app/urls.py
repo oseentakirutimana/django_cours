47e59dfc055ebe1eb75d189e3671417943de4718
osee <cishahayoosee@gmail.com>
2022-12-01 12:44:19 +0100

    modification de product

--- app/urls.py
+++ app/urls.py
@@ -33,11 +33,6 @@ urlpatterns = [
    path('products/edit/<int:id>', views.products.edit, name='products_edit'),
    path('products/delete/<int:id>', views.products.delete, name='products_delete'),
 
-   path('customers/',views.customers.index, name='customers_index'),
-   path('customers/create',views.customers.create, name='customers_create'),
-   path('customers/save',views.customers.save,name='customers_save'),
-   path('customers/edit/<int:id>', views.customers.edit, name='customers_edit'),
-   path('customers/delete/<int:id>', views.customers.delete, name='customers_delete'),
    
    
 ]
